9cd3ce3c2591a7846f5c85720e11ce6109bce183
arctan95 <jaeqentan@gmail.com>
2024-02-23 09:18:01 +0000

    use asdf-vm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -21,17 +21,9 @@
   ];
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".asdf/lib".source = "${pkgs.asdf-vm}/share/asdf-vm/lib";
+    ".asdf/completions".source = "${pkgs.asdf-vm}/share/asdf-vm/completions";
+    ".asdf/asdf.sh".source = "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh";
   };
 
   home.sessionVariables = {
@@ -64,7 +56,5 @@
       plugins = [ "asdf" "git" "pass" ];
       theme = "robbyrussell";
     };
-    
-    initExtra = ". ${pkgs.asdf-vm}/share/asdf-vm/asdf.sh";
   };
 }
